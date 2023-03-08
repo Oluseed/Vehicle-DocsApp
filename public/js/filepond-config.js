c1f0b07817e263442747ac1f27004ad4d2fcdcdf
Cysteine12 <gbengapeace2001@gmail.com>
2023-03-08 15:13:03 +0100

    update filepond version

--- public/js/filepond-config.js
+++ public/js/filepond-config.js
@@ -1,19 +1,15 @@
-Filepond.registerPlugin(
-    FilepondPluginImagePreview,
-    FilepondPluginImageResize,
-    FilepondPluginFileEncode,
-)
+document.addEventListener('DOMContnentLoaded', () => {
+    Filepond.registerPlugin(
+        FilepondPluginImagePreview,
+        FilepondPluginImageResize,
+        FilepondPluginFileEncode
+    )
 
-Filepond.setOptions({
-    stylePanelAspectRatio: 150 /100,
-    imageResizeTargetWidth: 100,
-    imageResizeTargetHeight: 150
+    Filepond.setOptions({
+        stylePanelAspectRatio: 150 /100,
+        imageResizeTargetWidth: 100,
+        imageResizeTargetHeight: 150
+    })
+
+    Filepond.parse(document.body)
 })
-
-// const pond = Filepond.create({
-//     multiple: false,
-//     name: 'filepond'
-// })
-
-// document.body.appendChild(pond.element)
-Filepond.parse(document.body)
