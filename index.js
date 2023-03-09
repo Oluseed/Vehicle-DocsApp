b3fda3f427aa5ba273afb8aac636b7a1fa5559fb
Cysteine <74334016+Cysteine12@users.noreply.github.com>
2023-03-09 12:44:59 +0100

    Update index.js

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ connectDB()
 
 
 //=======Middleware======//
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json({ limit: '10mb' }))
+app.use(express.urlencoded({ limit: '10mb', extended: true, parameterLimit: 10000 }))
 app.use(session({
     secret: process.env.SESS_SECRET,
     resave: false,
